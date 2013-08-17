b649027cba6ed78f01acfd13ab57c27d31e0992a
Sami <samiahmed74@gmail.com>
2013-08-17 08:44:27 +0000

    added

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('My name is Sami!');
+ fs.readFileSync(index.html);
+ response.send(buf.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
