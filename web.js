8a7a599e6b97fdb1c76f0abde22c40ee4a59a342
Sami <samiahmed74@gmail.com>
2013-08-17 08:58:32 +0000

    added

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
+buf = new Buffer(2000);
 app.get('/', function(request, response) {
- fs.readFileSync(index.html);
- response.send(buf.toString('utf-8');
+ buf=fs.readFileSync("index.html");
+ response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
